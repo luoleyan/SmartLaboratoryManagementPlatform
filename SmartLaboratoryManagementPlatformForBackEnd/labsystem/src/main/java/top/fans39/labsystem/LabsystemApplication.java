1362a1893a47163e652a5f83a3764e0964c74d87
luoleyan <105762083+luoleyan@users.noreply.github.com>
2024-04-10 11:16:08 +0800

    后端搭建

---
+++ SmartLaboratoryManagementPlatformForBackEnd/labsystem/src/main/java/top/fans39/labsystem/LabsystemApplication.java
@@ -0,0 +1,13 @@
+package top.fans39.labsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabsystemApplication.class, args);
+    }
+
+}
---
+++ SmartLaboratoryManagementPlatformForBackEnd/labsystem/src/test/java/top/fans39/labsystem/LabsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fans39.labsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
